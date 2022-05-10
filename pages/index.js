117b263e94e3ba077f2221b46ad9fda9bcc6c615
Beltrame01 <beltrame.vitor1@gmail.com>
2022-05-09 23:52:09 -0300

    alteração página

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,8 @@
-import { useState } from 'react';
-
 function Home()
 {
-    return (
-        <div>
-            <h1>HOME</h1>
-            <Contador />
-        </div>
-    )
+    return <h1>HOME</h1>
     
 }
 
-function Contador()
-{
-    const [contador,setContador] = useState("");
-
-        function adicionarContador() 
-        {
-            if (contador=="")
-            {
-                setContador(1);
-            }
-            else
-            {
-                setContador(parseInt(contador) + 1);
-            }
-            
-        }
-
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    )
-}
 
 export default Home
